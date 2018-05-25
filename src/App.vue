6b6c3e2ad4859121366cda959c93402f2ab58ff7
Chris Beach <beachcoding47@gmail.com>
2018-05-25 15:50:45 -0500

    #8 Basic structure of a bootstrap vue carousel in place

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <search-results msg="Equella Searching Demo" searchResultTypes="mini courses" baseUrl="https://equella.unicon.net/demo-oa18-up-eq-integ" collectionId="1ffa9507-4313-4f4f-9f6c-b923a897c515" resultIcon="_THUMBS/promo.jpeg_PREVIEW_.jpeg" resultLandingPage="index.html"/>
+    <!--<search-results msg="Equella Searching Demo" searchResultTypes="mini courses" baseUrl="https://equella.unicon.net/demo-oa18-up-eq-integ" collectionId="1ffa9507-4313-4f4f-9f6c-b923a897c515" resultIcon="_THUMBS/promo.jpeg_PREVIEW_.jpeg" resultLandingPage="index.html"/>-->
+    <image-carousel baseUrl="https://equella.unicon.net/demo-oa18-up-eq-integ" resultImage="_THUMBS/promo.jpeg_PREVIEW_.jpeg" resultLandingPage="index.html"/>
   </div>
 </template>
 
 <script>
-import SearchResults from "./components/SearchResults.vue";
+//import SearchResults from "./components/SearchResults.vue";
+import ImageCarousel from "./components/ImageCarousel.vue";
 
 export default {
   name: "app",
   components: {
-    "search-results": SearchResults
+    //"search-results": SearchResults,
+    "image-carousel": ImageCarousel,
   }
 };
 </script>
---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <b-carousel id="carousel1"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+    >
+
+      <!-- Text slides with image -->
+      <b-carousel-slide caption="First slide"
+                        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58">
+      </b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             src="https://picsum.photos/1024/480/?image=55" alt="image slot">
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
+          ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+
+    </b-carousel>
+
+    <p class="mt-4">
+      Slide #: {{ slide }}<br>
+      Sliding: {{ sliding }}
+    </p>
+
+  </div>
+</template>
+
+<style src="bootstrap/dist/css/bootstrap.css" />
+<style src="bootstrap-vue/dist/bootstrap-vue.css" />
+
+<script>
+import carousel from "bootstrap-vue/es/components/carousel/carousel";
+import slide from "bootstrap-vue/es/components/carousel/carousel-slide";
+
+export default {
+  components: {
+    "b-carousel": carousel,
+    "b-carousel-slide": slide
+  },
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      console.log(slide);
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      console.log(slide);
+      this.sliding = false;
+    }
+  }
+};
+</script>
